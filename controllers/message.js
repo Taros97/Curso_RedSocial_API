06bcd869d74e099c32b5df8f3b127f79af95ec2c
Néstor Pascual Antón <nestor_anton@hotmail.es>
2021-01-02 11:39:51 +0100

    Backend Inicial Terminado

--- controllers/message.js
+++ controllers/message.js
@@ -6,12 +6,113 @@ var mongoosePaginate = require('mongoose-pagination');
 var User = require('../models/user')
 var Follow = require('../models/follow')
 var Message = require('../models/message');
-const { models } = require('mongoose');
+var moment = require('moment');
+const { use } = require('../routes/message');
 
-function prueba(req, res){
-    return status(200).send({message : 'Prueba de Message'})
+function saveMessage(req, res){
+    var params = req.body;
+
+    if(!params.text && !params.receiver)
+        return res.status(200).send({message : 'Envia los datos necesarios'})
+
+    var message = new Message();
+    message.emitter = req.user.sub;
+    message.receiver = params.receiver;
+    message.text = params.text;
+    message.created_at = moment().unix();
+    message.viewed = 'false';
+
+    message.save((err, messageStored) =>{
+        if(err)
+            return res.status(599).send({message : 'Error en la peticion'})
+        if(!messageStored)
+            return res.status(404).send({message : 'Error al enviar el mensaje'})
+            
+        return res.status(200).send({message : messageStored})
+    })
+}
+
+function getReceivedMessages(req,res){
+    var userId = req.user.sub;
+
+    var page = 1
+    if(req.params.page)
+        page = req.params.page;
+
+    var itemsPerPage = 4;
+
+    Message.find({receiver : userId}).populate('emitter', 'name surname _id nick image').paginate(page,itemsPerPage,(err, messages ,total) =>{
+        if(err)
+        return res.status(599).send({message : 'Error en la peticion'})
+        if(!messages)
+            return res.status(404).send({message : 'No hay mensajes'})
+            
+        return res.status(200).send({
+            messages,
+            total,
+            page,
+            pages : Math.ceil(total/itemsPerPage)
+        })
+    })
+
+}
+
+function getEmmitmessages(req,res){
+    var userId = req.user.sub;
+
+    var page = 1
+    if(req.params.page)
+        page = req.params.page;
+
+    var itemsPerPage = 4;
+
+    Message.find({emitter : userId}).populate('emitter receiver', 'name surname _id nick image').paginate(page,itemsPerPage,(err, messages ,total) =>{
+        if(err)
+            return res.status(599).send({message : 'Error en la peticion'})
+        if(!messages)
+            return res.status(404).send({message : 'No hay mensajes'})
+            
+        return res.status(200).send({
+            messages,
+            total,
+            page,
+            pages : Math.ceil(total/itemsPerPage)
+        })
+    })
+
+}
+
+function getUnviewedMessages(req,res){
+    var userId = req.user.sub;
+
+    var page = 1
+    if(req.params.page)
+        page = req.params.page;
+
+    var itemsPerPage = 4;
+
+    Message.count({receiver : userId, viewed : 'false'}).exec().then((count) =>{
+        return res.status(200).send({'unviewed': count})
+    }).catch((err) => {
+        return res.status(500).send({message : 'Error en la peticion'})
+    });
+
+}
+
+function setViewedmessages(req, res){
+    var userid = req.user.sub;
+
+    Message.update({receiver : userid, viewed : 'false'}, {viewed: 'true'}, {"multi" : true} , (err, messagesUpdated) =>{
+        if(err)
+            return res.status(500).send({message : 'Error en la peticion'})
+        return res.status(200).send({messages: messagesUpdated})
+    })
 }
 
 module.exports = {
-    prueba,
+    saveMessage,
+    getReceivedMessages,
+    getEmmitmessages,
+    getUnviewedMessages,
+    setViewedmessages
 }
--- routes/message.js
+++ routes/message.js
@@ -9,6 +9,10 @@ var md_auth = require('../middlewares/authenticated')
 var multipart = require('connect-multiparty')
 var md_upload = multipart({uploadDir: './uploads/publications'})
 
-api.get('/prueba', md_auth.ensureAuth, MessageController.prueba);
+api.post('/message', md_auth.ensureAuth, MessageController.saveMessage);
+api.get('/my-messages/:page?', md_auth.ensureAuth, MessageController.getReceivedMessages);
+api.get('/messages/:page?', md_auth.ensureAuth, MessageController.getEmmitmessages);
+api.get('/unviewed-messages', md_auth.ensureAuth, MessageController.getUnviewedMessages);
+api.get('/set-viewed-messages', md_auth.ensureAuth, MessageController.setViewedmessages);
 
 module.exports = api;
